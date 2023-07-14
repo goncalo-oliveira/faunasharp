0d83e8555f13e423eb715badb1b2f1a70639396f
Goncalo Oliveira <goncalo.a.oliveira@gmail.com>
2023-07-14 19:24:04 +0100

    modified query arg order

--- src/QueryWithArgsClientExtensions.cs
+++ src/QueryWithArgsClientExtensions.cs
@@ -5,14 +5,14 @@ namespace FaunaDB;
 
 public static class ClientQueryWithJsonArgs
 {
-    public static Task<FaunaResponse> QueryAsync( this FaunaClient client, object arg1, Func<string, string> query, JsonSerializerOptions? jsonOptions = null )
+    public static Task<FaunaResponse> QueryAsync( this FaunaClient client, Func<string, string> query, object arg1, JsonSerializerOptions? jsonOptions = null )
     {
         var jsonArg1 = JsonSerializer.Serialize( arg1, jsonOptions );
 
         return client.QueryAsync( query( jsonArg1 ) );
     }
 
-    public static Task<FaunaResponse> QueryAsync( this FaunaClient client, object arg1, object arg2, Func<string, string, string> query, JsonSerializerOptions? jsonOptions = null )
+    public static Task<FaunaResponse> QueryAsync( this FaunaClient client, Func<string, string, string> query, object arg1, object arg2, JsonSerializerOptions? jsonOptions = null )
     {
         var jsonArg1 = JsonSerializer.Serialize( arg1, jsonOptions );
         var jsonArg2 = JsonSerializer.Serialize( arg2, jsonOptions );
@@ -20,7 +20,7 @@ public static class ClientQueryWithJsonArgs
         return client.QueryAsync( query( jsonArg1, jsonArg2 ) );
     }
 
-    public static Task<FaunaResponse> QueryAsync( this FaunaClient client, object arg1, object arg2, object arg3, Func<string, string, string, string> query, JsonSerializerOptions? jsonOptions = null )
+    public static Task<FaunaResponse> QueryAsync( this FaunaClient client, Func<string, string, string, string> query, object arg1, object arg2, object arg3, JsonSerializerOptions? jsonOptions = null )
     {
         var jsonArg1 = JsonSerializer.Serialize( arg1, jsonOptions );
         var jsonArg2 = JsonSerializer.Serialize( arg2, jsonOptions );
